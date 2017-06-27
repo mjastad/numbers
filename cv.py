2e5d402ac5c5bb5b60aee1ddfd2d6048653ecb01
Michael Jastad <mjastad@netapp.com>
2017-06-27 16:11:42 -0700

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+'''
+Python program to convert decimal number into binary, octal, and hexadecimal number system
+
+Usage:
+    Valid inputs are positive integer values:
+
+        $ python cv.py 20
+'''
+
+import sys
+
+__author__ = "M. Jastad"
+__copyright__ = "Copyright(c) 2015"
+__license__ = "USE-AS-IS"
+__version__ = "1.0.2"
+__maintainer__ = "M. Jastad"
+__email__ = "majastad@icloud.com"
+
+
+def main():
+
+
+    userInput = sys.argv[1]
+
+    try:
+        userInput = int(userInput)
+    except:
+        print "error: input must be numeric."
+        exit(0)
+
+    print "converted values of", userInput, '\n'
+
+    data = [[bin(userInput), ": binary"], [oct(userInput), ": octal"], [hex(userInput), ": hexadecimal"]]
+    col_width = max(len(word) for row in data for word in row) + 2
+    for row in data:
+        print "".join(word.ljust(col_width) for word in row)
+
+
+if __name__ == "__main__":
+    main()
---
+++ fib.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+'''
+Python program to display the Fibonacci sequence up to n-th term using recursive functions
+
+Usage:
+    Valid inputs are positive integer values:
+
+        $ python fib.py 10
+'''
+
+import sys
+
+__author__ = "M. Jastad"
+__copyright__ = "Copyright(c) 2015"
+__license__ = "USE-AS-IS"
+__version__ = "2.0.4"
+__maintainer__ = "M. Jastad"
+__email__ = "majastad@icloud.com"
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+
+
+def main():
+
+    userInput = sys.argv[1]
+
+    try:
+        userInput = int(userInput)
+    except:
+        print "error: ", userInput, "is non-numeric."
+        exit(0)
+
+    if userInput <= 0:
+        print("error: enter a positive integer")
+        exit(0)
+    else:
+        print("Fibonacci sequence:")
+        for i in range(userInput):
+            print(fibonacci(i))
+
+if __name__ == "__main__":
+    main()
